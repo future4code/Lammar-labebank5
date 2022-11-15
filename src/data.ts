2a36a03ceff94105a7254a1cde16320204c0da08
SantiagoOliveira22 <santiagoc.oliveira@gmail.com>
2022-11-15 18:59:53 -0300

    Feito adiciona saldo

--- src/data.ts
+++ src/data.ts
@@ -1,37 +1,25 @@
-export type Conta = {
-    nome: string,
-    cpf: string,
-    dataDeNascimento: string,
-    saldo: number,
-    extrato: Extrato[]
-}
+import * as tipo from "./type";
 
-export type Extrato = {
-    valor: number,
-    data: string,
-    descricao: string
-}
-
-export const clientes: Conta[] = [
-    {
-        nome: "João",
-        cpf: "123.456.789-00",
-        dataDeNascimento: "10/10/2000",
-        saldo: 0,
-        extrato: []
-    },
-    {
-        nome: "Pedro",
-        cpf: "123.456.789-01",
-        dataDeNascimento: "10/10/2000",
-        saldo: 0,
-        extrato: []
-    },
-    {
-        nome: "Paulo",
-        cpf: "123.456.789-02",
-        dataDeNascimento: "10/10/2000",
-        saldo: 0,
-        extrato: []
-    }
-]
+export const clientes: tipo.Conta[] = [
+  {
+    nome: "João",
+    cpf: "123.456.789-00",
+    dataDeNascimento: "10/10/2000",
+    saldo: 0,
+    extrato: [],
+  },
+  {
+    nome: "Pedro",
+    cpf: "123.456.789-01",
+    dataDeNascimento: "10/10/2000",
+    saldo: 0,
+    extrato: [],
+  },
+  {
+    nome: "Paulo",
+    cpf: "123.456.789-02",
+    dataDeNascimento: "10/10/2000",
+    saldo: 0,
+    extrato: [],
+  },
+];
---
+++ src/type.ts
@@ -0,0 +1,13 @@
+export type Conta = {
+  nome: string;
+  cpf: string;
+  dataDeNascimento: string;
+  saldo: number;
+  extrato: Extrato[];
+};
+
+export type Extrato = {
+  valor: number;
+  data: string;
+  descricao: string;
+};
